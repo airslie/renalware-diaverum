3c793c57b45a603c08e56620b72c7ff056e1a446
Tim Crowe <tim@woodpigeon.com>
2018-07-31 13:41:20 +0100

    Add timestamps to script to load accessmap

--- db/sql/map_access.sql
+++ db/sql/map_access.sql
@@ -1,57 +1,57 @@
-delete from renalware_diaverum.access_maps;
+truncate table  renalware_diaverum.access_maps;
 
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('RRAC', 1, 'right', 1); --
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('LRAC', 1, 'left', 1); --
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('RRAC', 1, 'right', 1, current_timestamp, current_timestamp);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('LRAC', 1, 'left', 1, current_timestamp, current_timestamp);
 
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('RRAD', 1, 'right', 2); --
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('LRAD', 1, 'left', 2); --
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('RRAD', 1, 'right', 2, current_timestamp, current_timestamp);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('LRAD', 1, 'left', 2, current_timestamp, current_timestamp);
 
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('RAB', 1, 'right', 3); --
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('LAB', 1, 'left', 3); --
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('RAB', 1, 'right', 3, current_timestamp, current_timestamp);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('LAB', 1, 'left', 3, current_timestamp, current_timestamp);
 
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('RHUB', 1, 'right', 4); --
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('LHUB', 1, 'left', 4); --
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('RHUB', 1, 'right', 4, current_timestamp, current_timestamp);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('LHUB', 1, 'left', 4, current_timestamp, current_timestamp);
 
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('RRAC', 3, 'right', 9);--
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('LRAC', 3, 'left', 9);--
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('RRAC', 3, 'right', 9, current_timestamp, current_timestamp);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('LRAC', 3, 'left', 9, current_timestamp, current_timestamp);
 
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('RRAD', 3, 'right', 10); --
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('LRAD', 3, 'left', 10); --
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('RRAD', 3, 'right', 10, current_timestamp, current_timestamp);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('LRAD', 3, 'left', 10, current_timestamp, current_timestamp);
 
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('RHUA', 3, 'right', 11); --
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('LHUA', 3, 'left', 11); --
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('RHUA', 3, 'right', 11, current_timestamp, current_timestamp);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('LHUA', 3, 'left', 11, current_timestamp, current_timestamp);
 
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('RTHI', 3, 'right', 16); --
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('LTHI', 3, 'left', 16); --
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('RTHI', 3, 'right', 16, current_timestamp, current_timestamp);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('LTHI', 3, 'left', 16, current_timestamp, current_timestamp);
 
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('RHUB', 3, 'right', 18); --
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('LHUB', 3, 'left', 18);--
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('RHUB', 3, 'right', 18, current_timestamp, current_timestamp);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('LHUB', 3, 'left', 18, current_timestamp, current_timestamp);
 
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('RIJ', 5, 'right', 41); --
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('LIJ', 5, 'left', 41); --
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('RIJ', 5, 'right', 41, current_timestamp, current_timestamp);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('LIJ', 5, 'left', 41, current_timestamp, current_timestamp);
 
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('RSUB', 5, 'right', 42); --
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('LSUB', 5, 'left', 42); --
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('RSUB', 5, 'right', 42, current_timestamp, current_timestamp);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('LSUB', 5, 'left', 42, current_timestamp, current_timestamp);
 
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('RFEM', 5, 'right', 43); --
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('LFEM', 5, 'left', 43); --
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('RFEM', 5, 'right', 43, current_timestamp, current_timestamp);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('LFEM', 5, 'left', 43, current_timestamp, current_timestamp);
 
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('RIJ', 4, 'right', 44);--
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('LIJ', 4, 'left', 44);--
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('RIJ', 4, 'right', 44, current_timestamp, current_timestamp);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('LIJ', 4, 'left', 44, current_timestamp, current_timestamp);
 
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('RSUB', 4, 'right', 45);--
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('LSUB', 4, 'left', 45);--
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('RSUB', 4, 'right', 45, current_timestamp, current_timestamp);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('LSUB', 4, 'left', 45, current_timestamp, current_timestamp);
 
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('RFEM', 4, 'right', 46); --
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('LFEM', 4, 'left', 46); --
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('RFEM', 4, 'right', 46, current_timestamp, current_timestamp);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('LFEM', 4, 'left', 46, current_timestamp, current_timestamp);
 
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('RHUC', 3, 'right', 58); --
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('LHUC', 3, 'left', 58); --
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('RHUC', 3, 'right', 58, current_timestamp, current_timestamp);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('LHUC', 3, 'left', 58, current_timestamp, current_timestamp);
 
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('RHUA', 2, 'right', 61); --
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('LHUA', 2, 'left', 61); --
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('RHUA', 2, 'right', 61, current_timestamp, current_timestamp);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('LHUA', 2, 'left', 61, current_timestamp, current_timestamp);
 
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('RHUB', 2, 'right', 62); --
-insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id) VALUES ('LHUB', 2, 'left', 62);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('RHUB', 2, 'right', 62, current_timestamp, current_timestamp);
+insert into renalware_diaverum.access_maps (diaverum_location_id, diaverum_type_id, side, access_type_id, created_at, updated_at) VALUES ('LHUB', 2, 'left', 62, current_timestamp, current_timestamp);
 
 --select * from renalware_diaverum.access_maps;
