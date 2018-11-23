7b6dd3e6f5f9c51a92a4c092ad04f1aed871716a
Tim Crowe <tim@woodpigeon.com>
2018-11-23 15:49:38 +0000

    Add notes to imported DNA sessions

--- app/models/renalware/diaverum/incoming/nodes/journal_entry.rb
+++ app/models/renalware/diaverum/incoming/nodes/journal_entry.rb
@@ -6,8 +6,14 @@ module Renalware
   module Diaverum
     module Incoming
       module Nodes
-        # Wraps an incoming Patient XML node
+        # Wraps an JournalEntry XML node which has have Date, Name, Type and Text elements.
+        # Type can be e.g. 'Treatmnt Note' or 'Daily Note'
         class JournalEntry < Node
+          # We use this accessor to indicate the JournalEntry's Text content has been included in
+          # the notes field of a session (or potentially but not >1 sessions on the same date)
+          # because the Treatment/Date == JournalEntry/Date.
+          attr_accessor :included_in_session_notes
+          alias :included_in_session_notes? :included_in_session_notes
         end
       end
     end
