edddaf39af45052a37551cba24a5f2773a7237d8
Tim Crowe <tim@woodpigeon.com>
2023-01-15 21:08:38 +0000

    Fix Sftp => SFTP inflection issue

--- spec/models/renalware/diaverum/outgoing/forward_hl7_job_spec.rb
+++ spec/models/renalware/diaverum/outgoing/forward_hl7_job_spec.rb
@@ -5,12 +5,12 @@ require "rails_helper"
 module Renalware
   module Diaverum
     RSpec.describe Outgoing::ForwardHL7Job do
-      it "delegates to ForwardHL7ToDiaverumViaSftp" do
-        allow(Outgoing::ForwardHL7ToDiaverumViaSftp).to receive(:call)
+      it "delegates to ForwardHL7ToDiaverumViaSFTP" do
+        allow(Outgoing::ForwardHL7ToDiaverumViaSFTP).to receive(:call)
 
         expect(described_class.new.perform(transmission: nil))
 
-        expect(Outgoing::ForwardHL7ToDiaverumViaSftp).to have_received(:call)
+        expect(Outgoing::ForwardHL7ToDiaverumViaSFTP).to have_received(:call)
       end
     end
   end
